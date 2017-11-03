6eca80751fd45bc27a96cf04c7357dadf0a16075
DESKTOP-UDPNUO3\chenj <jiaxuan.chen@alumni.ubc.ca>
2017-11-03 15:56:57 -0700

    first commit!!!!!

--- app/init-db.js
+++ app/init-db.js
@@ -1,6 +1,3 @@
-/**
- * Created by kristys on 2017-10-19.
- */
 const connection = require('./connection');
 const async = require('async');
 
@@ -20,13 +17,28 @@ async.waterfall([
         );
     },
     function (callback) {
+        connection.query(
+            `create table Hospital (
+	            hID	       varchar(30),
+                name      varchar(30),
+                location   varchar(30),
+                primary key (hID)
+            )`,
+            function (err, result) {
+                if (err && err.code !== 'ER_TABLE_EXISTS_ERROR') callback(err);
+                else callback(null);
+            }
+        );
+    },
+    function (callback) {
         connection.query( // TODO: add the hospital table and add foreign key constraint here
             `CREATE TABLE Recruiter (
                 name varchar(30),
                 email varchar(30),
                 pw varchar(30),
                 hID char(6) NOT NULL,
-                PRIMARY KEY(email)
+                PRIMARY KEY(email),
+                foreign key (hID) references Hospital(hID)
             )`,
             function (err, result) {
                 if (err && err.code !== 'ER_TABLE_EXISTS_ERROR') callback(err);

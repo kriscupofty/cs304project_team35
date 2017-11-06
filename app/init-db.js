37fa8594dd9e8fe98af4bd3cd2eda8fd26633057
DESKTOP-UDPNUO3\chenj <jiaxuan.chen@alumni.ubc.ca>
2017-11-06 02:13:40 -0800

    modified table definitions & populated the tables

--- app/init-db.js
+++ app/init-db.js
@@ -20,8 +20,8 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table Hospital (
-	            hID	       varchar(30),
-                name      varchar(30),
+	            hID	       int(11) unsigned auto_increment,
+                name       varchar(30),
                 location   varchar(30),
                 primary key (hID)
             )`,
@@ -34,7 +34,7 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table Program (
-	            hID	       varchar(30),
+	            hID	       int(11) unsigned,
                 pname      varchar(30),
                 numslots   integer,
                 primary key (hID, pname),
@@ -51,9 +51,9 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table Recruiter (
-                email      varchar(30),
-	            hID	       			varchar(30) not null,
                 name				varchar(30) not null,
+	            hID	       			int(11) unsigned not null,
+                email      varchar(30),
                 phone        		varchar(30) unique,
                 pw       			varchar(30) not null,
                 primary key (email),
@@ -70,17 +70,17 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table VacancyPosting (
-	            postingID		varchar(30),
+	            postingID		int(11) unsigned auto_increment,
                 recruiterEmail	varchar(30) not null,
                 pname           varchar(50) not null,
                 duration        varchar(30) not null,
                 specialty       varchar(50) not null,
                 deadline		date not null,
-                primary key (postingID),
+                    primary key (postingID),
                 foreign key (recruiterEmail) references Recruiter(email)
 	            on delete cascade
                 on update cascade
-           )`,
+            )`,
             function (err, result) {
                 if (err && err.code !== 'ER_TABLE_EXISTS_ERROR') callback(err);
                 else callback(null);
@@ -91,7 +91,7 @@ async.waterfall([
         connection.query(
             `create table RecruiterHospital (
                 recruiterEmail	varchar(30),
-                hID          	varchar(30) not null,
+                hID          	int(11) unsigned not null,
                 primary key (recruiterEmail),
                 foreign key (recruiterEmail) references Recruiter(email)
                 on delete cascade
@@ -116,7 +116,7 @@ async.waterfall([
                 phone             varchar(30) unique,
                 pw               varchar(30) not null,
                 primary key (email)
-           )`,
+            )`,
             function (err, result) {
                 if (err && err.code !== 'ER_TABLE_EXISTS_ERROR') callback(err);
                 else callback(null);
@@ -126,11 +126,11 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table Application (
-	            aID		varchar(30),
+	            aID		int(11) unsigned auto_increment,
                 form1path	varchar(30) not null,
                 form2path	varchar(30) not null,
                 primary key (aID)
-           )`,
+            )`,
             function (err, result) {
                 if (err && err.code !== 'ER_TABLE_EXISTS_ERROR') callback(err);
                 else callback(null);
@@ -141,9 +141,9 @@ async.waterfall([
         connection.query(
             `create table Applies (
 	            resEmail	varchar(30),
-                postingID	varchar(30),
+                postingID	int(11) unsigned,
                 time        date,
-                aID			varchar(30) not null,
+                aID			int(11) unsigned not null,
                 primary key (resEmail, postingID, time),
                 foreign key (resEmail) references ResidencyCandidate(email)
                 on delete cascade
@@ -154,7 +154,7 @@ async.waterfall([
                 foreign key (aID) references Application(aID)
                 on delete cascade
                 on update cascade
-           )`,
+            )`,
             function (err, result) {
                 if (err && err.code !== 'ER_TABLE_EXISTS_ERROR') callback(err);
                 else callback(null);
@@ -165,11 +165,11 @@ async.waterfall([
         connection.query(
             `create table Interviews (
 	            round		integer,
-                postingID	varchar(30),
-                resEmail      varchar(30),
+	            resEmail      varchar(30),
+                postingID	int(11) unsigned,
                 time        date not null,
                 location    varchar(30) not null,
-                primary key (round, postingID, resEmail),
+                primary key (round, resEmail,postingID),
 	            foreign key (postingID) references VacancyPosting(postingID)
                 on delete cascade
                 on update cascade,
@@ -186,8 +186,8 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table H_interviews_for_posting (
-                postingID	varchar(30),
-                hID      varchar(30) not null,
+                postingID	int(11) unsigned,
+                hID      int(11) unsigned not null,
                 primary key (postingID),
 	            foreign key (postingID) references VacancyPosting(postingID)
                 on delete cascade
@@ -205,8 +205,9 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table School (
-	            sid				varchar(30),
-                name			varchar(30) not null,
+	            sid				int(11) unsigned auto_increment,
+                name			varchar(50) not null,
+                province			varchar(30),
                 city			varchar(30),
                 primary key (sid)
             )`,
@@ -220,7 +221,7 @@ async.waterfall([
         connection.query(
             `create table CandidateAttended (
 	            resEmail        varchar(30),
-	            sid				varchar(30),
+	            sid				int(11) unsigned,
                 primary key (resEmail,sid)
             )`,
             function (err, result) {
@@ -232,7 +233,7 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table H_ranks (
-                hID		varchar(30),
+                hID		int(11) unsigned,
                 resEmail	varchar(30),
                 rank		integer not null,
                 primary key (hID, resEmail),
@@ -253,7 +254,7 @@ async.waterfall([
         connection.query(
             `create table R_ranks (
                 resEmail	varchar(30),
-	            hID		varchar(30),
+	            hID		int(11) unsigned,
                 rank		integer not null,
                 primary key (resEmail,hID),
                 foreign key (resEmail) references ResidencyCandidate(email)
@@ -272,9 +273,9 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table Offers (
-                postingID		varchar(30),
+                postingID		int(11) unsigned,
 	            resEmail		varchar(30),
-	            hID				varchar(30),
+	            hID				int(11) unsigned,
                 decision		varchar(30) not null,
                 primary key (postingID,resEmail),
                 foreign key (resEmail) references ResidencyCandidate(email)
@@ -296,9 +297,9 @@ async.waterfall([
     function (callback) {
         connection.query(
             `create table Contract (
-                postingID		varchar(30),
-	            hID				varchar(30),
+                postingID		int(11) unsigned,
 	            resEmail		varchar(30),
+	            hID				int(11) unsigned,
                 compensation	numeric(9,2) not null,
                 primary key (postingID,resEmail),
                 foreign key (resEmail) references ResidencyCandidate(email)

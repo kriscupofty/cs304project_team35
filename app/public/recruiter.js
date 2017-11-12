f76155f5afc5775a3bd8584559d256c2e9d97cb4
KristyS <kristys@alumni.ubc.ca>
2017-11-11 18:16:02 -0800

    added sidebar to dashboards

--- app/public/recruiter.js
+++ app/public/recruiter.js
@@ -1,1 +1,16 @@
-//Todo
+$(document).ready(function () {
+    $('main.container').toggle();
+    $.ajax({
+        url: "http://localhost:1234/isloggedin/admin",
+        contentType: "application/json; charset=utf-8",
+        type: "GET",
+        success: function (res) {
+            $('main.container').toggle();
+        },
+        error: function (err) {
+            $('#log').text('Log in');
+            $('main.container').empty().append("<h2>Please log in as a Recruiter first.</h2>").toggle();
+
+        }
+    });
+});
